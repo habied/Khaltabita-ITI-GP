f2061292cec6fc3e44a4b1cfc4c0a32467d1b642
unknown <sherin.reda15@gmail.com>
2023-06-24 15:56:07 +0300

    handling-cart-logout&handling-quantities-cart

--- Angular/src/app/checkout/checkout.component.ts
+++ Angular/src/app/checkout/checkout.component.ts
@@ -67,14 +67,18 @@ export class CheckoutComponent implements OnInit {
   finalOrder: Checkout | null = null;
   increaseItemQuantity(menuItemIndex: number) {
     ++this.order[menuItemIndex].qty;
-    this.menuService.itemsNumber$.next(this.order[menuItemIndex].qty);
+    this.menuService.itemsNumber$.next(this.menuService.itemsNumber$.value + 1);
     this.calculateSubtotal();
   }
-  decreaseItemQuantity(menuItemIndex: number) {
+  decreaseItemQuantity(menuItemIndex: number, item: ItemOrder) {
     if (this.order[menuItemIndex].qty - 1 > 0) {
       --this.order[menuItemIndex].qty;
-      this.menuService.itemsNumber$.next(this.order[menuItemIndex].qty);
+      this.menuService.itemsNumber$.next(
+        this.menuService.itemsNumber$.value - 1
+      );
       this.calculateSubtotal();
+    } else {
+      this.removeFromCart(item);
     }
   }
   // removeFromCart(id){
@@ -86,16 +90,20 @@ export class CheckoutComponent implements OnInit {
   removeFromCart(itemOrder: ItemOrder) {
     // Find the index of the itemOrder in the orders array
     const index = this.menuService.orders.indexOf(itemOrder);
-    
+
     // If the index is not -1 (i.e. itemOrder is in the orders array), remove the itemOrder from the orders array using splice()
     if (index !== -1) {
       this.menuService.orders.splice(index, 1);
-      
+
       // Update the itemsNumber$ observable by subtracting the quantity of the removed itemOrder from the current value
-      this.menuService.itemsNumber$.next(this.menuService.itemsNumber$.getValue() - itemOrder.qty);
-      
+      this.menuService.itemsNumber$.next(
+        this.menuService.itemsNumber$.getValue() - itemOrder.qty
+      );
+
       // Update the ordersList property to reflect the updated contents of the orders array
-      this.order = this.menuService.orders.filter(order => order.menuItem.name !== itemOrder.menuItem.name);
+      this.order = this.menuService.orders.filter(
+        (order) => order.menuItem.name !== itemOrder.menuItem.name
+      );
     }
   }
   calculateSubtotal() {
@@ -106,10 +114,9 @@ export class CheckoutComponent implements OnInit {
   }
   //confirmOrder() {}
 
-  openPopup(){
-    this.popup.open(PopupComponent,{
+  openPopup() {
+    this.popup.open(PopupComponent, {
       width: '400px',
-    })
-
+    });
   }
 }
